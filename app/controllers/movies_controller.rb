5b8c90bf00e1def3d8fa316bfdb5e68eda905ad2
Joshua Szachar <jszacha1@binghamton.edu>
2021-03-16 23:18:32 +0000

    second commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,8 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = Movie.all_ratings
   end
 
   def new
